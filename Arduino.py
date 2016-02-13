1c5a2f856e92a7a469a04577869897c881982cfe
Simon Vahr <simon.tang.vahr@gmail.com>
2016-02-13 17:29:10 +0530

    Bluetooth Woo!

--- Arduino.py
+++ Arduino.py
@@ -1,9 +1,9 @@
 # Script created by Group 14 in the team
-from serial import * 
+from serial import *
 import argparse
 
-PORT_BLUE = '/dev/tty.usbmodem1421'
-PORT_RED = '/dev/tty.usbserial-DA00SY6T'
+PORT_BLUE = '/dev/tty.HC-05-DevB'
+PORT_RED = '/dev/tty.HC-05-DevB-1'
 
 # Parse the inputs
 ap = argparse.ArgumentParser()
@@ -18,13 +18,12 @@ temp = args["temp"]
 
 if (target == "red"):
 	ard_red = Serial(PORT_RED, 9600)
-	ard_red.write(temp)
+	ard_red.write("T" + temp)
 elif (target == "blue"):
 	ard_blue = Serial(PORT_BLUE, 9600)
-	ard_blue.write(temp)
+	ard_blue.write("T" + temp)
 else: # Both boards
 	ard_blue = Serial(PORT_BLUE, 9600)
 	ard_red = Serial(PORT_RED, 9600)
-
-	ard_blue.write(temp)
-	ard_red.write(temp)
+	ard_blue.write("T" + temp)
+	ard_red.write("T" + temp)
